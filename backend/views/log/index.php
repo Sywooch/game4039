65d6ab0106955b77b3f8b6dddf2b68e9da3cb5fe
buuug7 <youpp@126.com>
2016-03-04 14:41:19 +0800

    修改了favicon.ico

--- backend/views/log/index.php
+++ backend/views/log/index.php
@@ -49,7 +49,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
             [
                 'class' => 'yii\grid\ActionColumn',
-                'template'=>'{view}{delete}'
+                'template'=>'{view} {delete}'
             ]
         ]
     ]); ?>
