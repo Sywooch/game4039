9c8e3c466f2f68e9bbe32797cd0cf6002d218ffd
buuug7 <youpp@126.com>
2016-08-17 18:01:53 +0800

    修复

--- frontend/web/config.inc.php
+++ frontend/web/config.inc.php
@@ -6,14 +6,14 @@ define('DISCUZ_DBTABLEPRE', '`dz`.pre_');
 
 define('UC_CONNECT', 'mysql');
 define('UC_DBHOST', 'localhost');
-define('UC_DBUSER', 'root');
+define('UC_DBUSER', 'gansutianqi2016');
 define('UC_DBPW', 'root');
 define('UC_DBNAME', 'dz');
 define('UC_DBCHARSET', 'utf8');
 define('UC_DBTABLEPRE', '`dz`.pre_ucenter_');
 define('UC_DBCONNECT', '0');
 define('UC_KEY', '123456');
-define('UC_API', 'http://bbs.game4039.dev/uc_server');
+define('UC_API', 'http://4.jq2.com/uc_server');
 define('UC_CHARSET', 'utf-8');
 define('UC_IP', '');
 define('UC_APPID', '2');
